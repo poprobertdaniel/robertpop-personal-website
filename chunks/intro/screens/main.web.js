052b682947e37018f66f3b2aedef19624ee3a46c
Robert Pop <poprobertdaniel22@gmail.com>
2018-12-24 13:53:42 +0200

    reset and working tweeter geter

--- chunks/intro/screens/main.web.js
+++ chunks/intro/screens/main.web.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { Screen } from 'react-dom-chunky'
 
 export default class MainScreen extends Screen {
-  constructor (props) {
+  constructor(props) {
     super(props)
-    this.state = { ...this.state }
+    this.state = { ...this.state, tweets: null }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     super.componentDidMount()
+    fetch('https://twitter-nodejs-kxlmyxwouc.now.sh/timeline?user_id=robipop22')
+      .then(response => response.json())
+      .then(resp => {
+        const { tweets } = resp
+        this.setState({ tweets })
+      })
+      .catch(err => console.log('err', err))
   }
 
-  components () {
+  components() {
     return super.components().concat([])
   }
 }
